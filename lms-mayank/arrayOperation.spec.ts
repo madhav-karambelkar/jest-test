f8c0c963237526a490bff129a12c09e64c455b4d
mayankV99 <mayank.verma@lmsin.com>
2024-05-09 23:37:23 +0530

    Implemented: functions to perform basic operations on arrays:- addition, subtraction, multiplication & transpose

--- lms-mayank/arrayOperation.spec.ts
+++ lms-mayank/arrayOperation.spec.ts
@@ -5,14 +5,51 @@ import {
   transposeArray,
 } from "./arrayOperation";
 
-describe("operations on arrays such as addition, subtraction, multiplication, and transpose.", () => {
-//   test.each([[]])(
-//     "adds $i + $a to equal $outcome",
-//     (...args) => {
-//       console.log("🚀 ~ describe ~ args:", args);
-//     }
-//   );
-  test("Both arrays should be empty on addition", ()=>{
-    expect(addArrays([1,2,3],[4, 5, 6])).toBe([5, 7, 9])
+beforeAll(()=> {
+  console.log("Running test");
+})
+
+describe("Addition of two array elements.", () => {
+  test("Both arrays may be []", ()=>{
+    expect(addArrays([],[])).toEqual([])
   });
+
+  test("Both arrays elements addition", ()=>{
+    expect(addArrays([1,2,3],[4, 5, 6])).toEqual([5, 7, 9])
+  });
+
+
 });
+
+describe("Subtraction of two array elements.", () => {
+    test("Both arrays may be []", ()=>{
+      expect(subtractArrays([],[])).toEqual([])
+    });
+  
+    test("Both arrays elements Subtraction", ()=>{
+      expect(subtractArrays([4, 5, 6],[1,2,3])).toEqual([3, 3, 3])
+    });
+  
+  });
+
+  describe("Multiplication of two array elements.", () => {
+      test.skip("Both arrays may be []", ()=>{
+        expect(multiplyArrays([],[])).toEqual([])
+      });
+    
+      test("Both arrays elements Multiplication", ()=>{
+        expect(multiplyArrays([1,2,3],[4, 5, 6])).toEqual([4, 10, 18])
+      });
+    
+});
+
+describe("Transpose of 2D array.", () => {
+    test("arrays may be [[],[]]", ()=>{
+      expect(transposeArray([[],[]])).toEqual([])
+    });
+  
+    test("Both arrays elements addition", ()=>{
+      expect(transposeArray([[1,2], [3,4]])).toEqual([[1,3], [2,4]])
+    });
+  
+  });
