4e46ba265f754fb45fcd7c8f9b57af3e494b6e94
nehakochar <neha.kochar@lmsin.com>
2024-05-15 13:19:26 +0530

    Update fizzBuzz.spec.ts

--- fizzBuzz.spec.ts
+++ fizzBuzz.spec.ts
@@ -1,19 +1,19 @@
 const fizzBuzz = require("./fizzBuzz");
 
 describe("FizzBuzz Tests", () => {
-  it('returns "Fizz" if the number is divisible by 3', () => {
+  it('"Fizz" if the number is divisible by 3', () => {
     expect(fizzBuzz(9)).toBe("Fizz");
   });
 
-  it('returns "Buzz" if the number is divisible by 5', () => {
+  it('"Buzz" if the number is divisible by 5', () => {
     expect(fizzBuzz(10)).toBe("Buzz");
   });
 
-  it('returns "FizzBuzz" if the number is divisible by both 3 and 5', () => {
+  it('"FizzBuzz" if the number is divisible by both 3 and 5', () => {
     expect(fizzBuzz(15)).toBe("FizzBuzz");
   });
 
-  it("returns the number itself if it's not divisible by 3 or 5", () => {
+  it("the number itself if it's not divisible by 3 or 5", () => {
     expect(fizzBuzz(7)).toBe("7");
   });
 });
